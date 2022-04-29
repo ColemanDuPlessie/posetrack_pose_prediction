945a7bb1ec1191011f51433d57a4261792c3f6c3
ColemanDuPlessie <coleyduplessie@gmail.com>
2022-04-29 12:40:14 -0400

    Made KitchenTransformerTest.py functional
    The decoding layer is still non-functional.

--- kitchen_pose_prediction/KitchenTransformerTest.py
+++ kitchen_pose_prediction/KitchenTransformerTest.py
@@ -13,9 +13,15 @@ from torch.autograd import Variable
 from sklearn.preprocessing import MinMaxScaler
 from ParseKitchenC3D import load_and_preprocess_mocap
 
-seq_length = 3 # TODO 100
-max_seq_length = 4 # TODO 1800
+min_seq_length = 3 # TODO 100
 predict_length = 1
+
+input_size = 198
+
+def generate_square_subsequent_mask(sz):
+    mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+    return mask
 
 def normalize_data(examples):
     sc = MinMaxScaler()
@@ -40,50 +46,42 @@ def load_data(filename):
     
     print("Converting train set to Tensor...")
     
-    train_data = Variable(torch.Tensor(data[0:train_size]))
+    train_data = Variable(torch.Tensor(data[0:train_size])).reshape(1, -1, input_size)
     
     print("Converting test set to Tensor...")
     
-    test_data = Variable(torch.Tensor(data[train_size:]))
+    test_data = Variable(torch.Tensor(data[train_size:])).reshape(1, -1, input_size)
     
-    return train_data, test_data
+    return train_data[:, :500], test_data[:, :200] # TODO I have manually limited the length of the train and test data for quick testing.
 
 class Transformer(nn.Module):
-    def __init__(self, hidden_size = 64, heads = 4, frame_dimension = 1, encoder_layers = 2, decoder_layers = 2):
+    def __init__(self, hidden_size = 64, heads = 4, frame_dimension = 1, layers = 2):
         super(Transformer, self).__init__()
         self.hidden_size = hidden_size
         self.input_size = frame_dimension
         self.encoding = nn.Linear(frame_dimension, hidden_size)
-        self.encoder = nn.TransformerEncoder(nn.TransformerEncoderLayer(hidden_size, heads, hidden_size, batch_first=True), encoder_layers)
-        self.decoder = nn.TransformerDecoder(nn.TransformerDecoderLayer(hidden_size, heads, hidden_size, batch_first=True), encoder_layers)
-
+        self.transformer = nn.TransformerEncoder(nn.TransformerEncoderLayer(hidden_size, heads, hidden_size, batch_first=True), layers)
         
-    def forward(self, y, pre_output_len=1, max_input_len=100):
-        outputs = []
-        frames = y.split(1, dim=0)
-        frames = [self.encoding(frame) for frame in frames]
-        for i in range(len(frames)):
-            if i % 25 == 0: print(i)
-            network_input = torch.stack(frames[i+1-max_input_len:i+1] if i >= max_input_len else frames[:i+1])
-            output = self.decoder(network_input, self.encoder(network_input))
-            if i >= pre_output_len: outputs.append(output)
+    def forward(self, y, pre_output_len=1):
+        frames = self.encoding(y)
+        mask = generate_square_subsequent_mask(frames.size()[1])
+        ans = self.transformer(frames, mask)[:, pre_output_len:]
+        reversed_encoding = self.encoding.weight # TODO .t()
+        assert y.equal(torch.matmul(frames, reversed_encoding))
+        ans = torch.matmul(ans, reversed_encoding)
 
-        outputs = [torch.matmul(self.encoding.weight.t(), output) for output in outputs] # This line is equivalent to multiplying by the transpose of the encoding layer's weight matrix.
-        outputs = torch.stack(outputs)
-        return outputs
+        return ans
 
 if __name__ == "__main__":
     num_epochs = 2
     learning_rate = 0.01
-    
-    input_size = 198
     
     hidden_size = 4
     num_layers = 2
     
     num_classes = 198
     
-    network = Transformer(hidden_size, 4, input_size, math.ceil(num_layers/2), math.floor(num_layers/2))
+    network = Transformer(hidden_size, 4, input_size, num_layers)
     
     criterion = torch.nn.MSELoss()    # mean-squared error for regression
     optimizer = torch.optim.Adam(network.parameters(), lr=learning_rate)
@@ -98,14 +96,13 @@ if __name__ == "__main__":
     # Train the model
     for epoch in range(num_epochs):
         network.train()
-        outputs = network(train_data[:-predict_length], seq_length, max_seq_length)
-        outputs = outputs.reshape(outputs.size(0), outputs.size(2))
+        outputs = network(train_data[:, :-predict_length], min_seq_length)
         
         optimizer.zero_grad()
         gc.collect()
         
         # obtain the loss function
-        loss = criterion(outputs, train_data[seq_length+predict_length:])
+        loss = criterion(outputs, train_data[:, min_seq_length+predict_length:])
         
         loss.backward()
         
@@ -117,9 +114,8 @@ if __name__ == "__main__":
         network.eval()
         
         with torch.no_grad():
-            train_predict = network(test_data[:-predict_length], seq_length, max_seq_length)
-            train_predict = train_predict.reshape(train_predict.size(0), train_predict.size(2))
-            test_loss = criterion(train_predict, test_data[seq_length+predict_length:])
+            train_predict = network(test_data[:, :-predict_length], min_seq_length)
+            test_loss = criterion(train_predict, test_data[:, min_seq_length+predict_length:])
             test_losses.append(test_loss.item())
             if epoch % 1 == 0:
                 print("Epoch: %d, train loss: %1.5f, test loss: %1.5f" % (epoch, loss.item(), test_loss.item()))
