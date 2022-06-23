f9ac5b468d2fd15cebfe354ddf43a1c447c13b12
ColemanDuPlessie <coleyduplessie@gmail.com>
2022-06-23 11:24:30 -0400

    Fixed up KitchenModelComparisonTest.py
    It is now almost ready to run (the only thing left is to put it on CUDA). I've greatly simplified the process for adding more models. I've also told it to train on the whole dataset, instead of just brownies1. To run it, the only things you need to change are the numbers of epochs and layers and the hidden size. (You can find those by searching for "TODO" in the file)

--- kitchen_pose_prediction/KitchenModelComparisonTest.py
+++ kitchen_pose_prediction/KitchenModelComparisonTest.py
@@ -4,15 +4,16 @@ This needs documentation at some point
 """
 
 import gc # This is an ugly hack
+import math
 import matplotlib.pyplot as plt
 import torch
 from torch.autograd import Variable
 from ParseKitchenC3D import load_and_prepare_mocaps, train_test_split
 from models.benchmarks import SimpleRepeater
 from models.Transformer_encoder import TransformerEncoder
-from models.Pyraformer.Pyraformer_SS import Model as Pyraformer
-from models.Pyraformer.Pyraformer_SS import pyraformer_params
-# TODO from models.Informer import Informer
+from models.Informer import Informer
+# TODO from models.Pyraformer.Pyraformer_SS import Model as Pyraformer
+# TODO from models.Pyraformer.Pyraformer_SS import pyraformer_params
 
 min_seq_length = 100
 predict_length = 1
@@ -28,21 +29,132 @@ def load_data(filenames, batch_size = 1024, train_qty = 0.67):
     """
     return train_test_split(Variable(load_and_prepare_mocaps(filenames, batch_size)), train_qty)
 
+class ModelWrapper:
+    """
+    This class is just a simple wrapper for various models.
+    """
+    
+    def __init__(self, model, name, optimizer_class, loss_function, optimizer_kwargs={}, store_losses=True):
+        self.model = model
+        self.name = name
+        self._optimizer = None if optimizer_class is None else optimizer_class(model.parameters(), **optimizer_kwargs)
+        self._loss_function = loss_function
+        self._store_losses = store_losses
+        if store_losses:
+            self.train_losses = []
+            self.test_losses = []
+    
+    def train(self, train_set, min_seq_len, predict_len=1, backprop=True):
+        self.model.train()
+        try:
+            output = self.model(train_set[:, :-predict_len, :], min_seq_len)
+        except TypeError:
+            print("This feature actually worked!")
+            output = self.model(train_set[:, :-predict_len, :])
+            output = output[:, min_seq_len-1:, :]
+        if backprop:
+            if self._optimizer is not None:
+                self._optimizer.zero_grad()
+            loss = self._loss_function(output, train_set[:, min_seq_len+predict_len:])
+            loss.backward()
+            if self._optimizer is not None:
+                self._optimizer.step()
+        if self._store_losses:
+            self.train_losses.append(loss.item())
+        return output
+    
+    def test(self, test_set, min_seq_len, predict_len=1):
+        self.model.eval()
+        with torch.no_grad():
+            try:
+                output = self.model(test_set[:, :-predict_len, :], min_seq_len)
+            except TypeError:
+                print("This feature actually worked!")
+                output = self.model(test_set[:, :-predict_len, :])
+                output = output[:, min_seq_len-1:, :]
+            loss = self._loss_function(output, test_set[:, min_seq_len+predict_len:])
+        if self._store_losses:
+            self.test_losses.append(loss.item())
+        return output
+    
+    def to(self, device):
+        self.model.to(device)
+    
+    def get_params_string(self):
+        return "%s has %d parameters." % (self.name, count_parameters(self.model))
+    
+    def get_losses_string(self):
+        return "%s train loss: %1.6f %s test loss: %1.6f" % (self.name, self.train_losses[-1], self.name, self.test_losses[-1])
+
+class MultiModelHandler:
+    """
+    This class is a collection of ModelWrappers (found above).
+    """
+    
+    def __init__(self, *model_wrappers):
+        self.networks = model_wrappers
+    
+    def train(self, train_set, min_seq_len, predict_len=1, backprop=True):
+        output = []
+        for network in self.networks:
+            output.append(network.train(train_set, min_seq_len, predict_len, backprop))
+        return output
+    
+    def test(self, test_set, min_seq_len, predict_len=1):
+        output = []
+        for network in self.networks:
+            output.append(network.test(test_set, min_seq_len, predict_len))
+        return output
+    
+    def to(self, device):
+        for network in self.networks: network.to(device)
+    
+    def get_params_string(self):
+        ans = ""
+        for network_idx, network in enumerate(self.networks):
+            ans += "Model %d: %s " % (network_idx, network.get_params_string())
+        return ans[:-1]
+    
+    def get_losses_string(self):
+        ans = ""
+        for network in self.networks:
+            ans += "%s " % network.get_losses_string()
+        return ans[:-1]
+    
+    def plot_losses_over_time(self):
+        for network in self.networks:
+            plt.plot(network.train_losses, label = "Train (%s)" % network.name)
+            plt.plot(network.test_losses, label = "Test (%s)" % network.name)
+        plt.legend()
+        plt.yscale('log')
+        plt.show()
+    
+    def save_models(self, filenames):
+        for idx, network in enumerate(self.networks):
+            if filenames[idx] is not None:
+                torch.save(network.model.state_dict(), filenames[idx])
+
 if __name__ == "__main__":
-    num_epochs = 2 # TODO
+    num_epochs = 50 # TODO
     learning_rate = 0.01
     batch_size = 1024
     positional_embedding_max_len = batch_size * 2
     
-    hidden_size = 1024
-    num_layers = 4
+    hidden_size = 1024 # TODO
+    num_layers = 4 # TODO
     
     num_classes = 153
     
-    network1 = TransformerEncoder(hidden_size, 8, input_size, num_layers, positional_embedding_max_len)
-    network2 = SimpleRepeater(input_size)
-    network3 = Pyraformer(pyraformer_params(num_classes, hidden_size, n_head=8, n_layer=num_layers, batch_size=batch_size, inner_hid=hidden_size, d_k=hidden_size/4, d_v=hidden_size/4, device='cpu')) # !!! Change device to cuda if you're running a model of any significant size.
+    networks = MultiModelHandler(ModelWrapper(TransformerEncoder(hidden_size, 8, input_size, num_layers, positional_embedding_max_len), "Transformer (encoder only)", torch.optim.Adam, torch.nn.MSELoss(), {"lr" : learning_rate}),
+                ModelWrapper(Informer(input_size, input_size, input_size, 1, d_model = hidden_size, n_heads = 8,
+                                         e_layers = math.ceil(num_layers/2), d_layers = math.floor(num_layers/2),
+                                         d_ff = hidden_size, activation = "relu", positional_embedding_max_len = positional_embedding_max_len), "Informer", torch.optim.Adam, torch.nn.MSELoss(), {"lr" : learning_rate}),
+                ModelWrapper(SimpleRepeater(input_size), "Benchmark", None, torch.nn.MSELoss()),)
+    
 # =============================================================================
+#     network1 = TransformerEncoder(hidden_size, 8, input_size, num_layers, positional_embedding_max_len)
+#     network2 = SimpleRepeater(input_size)
+#     network3 = Pyraformer(pyraformer_params(num_classes, hidden_size, n_head=8, n_layer=num_layers, batch_size=batch_size, inner_hid=hidden_size, d_k=hidden_size/4, d_v=hidden_size/4, device='cpu')) # !!! Change device to cuda if you're running a model of any significant size.
 #     network2 = Informer(input_size, input_size, input_size, 1,
 #                         d_model = hidden_size, n_heads = 8,
 #                         e_layers = math.ceil(num_layers/2),
@@ -50,83 +162,29 @@ if __name__ == "__main__":
 #                         d_ff = hidden_size, activation = "relu", device = torch.device("cpu"), 
 #                         positional_embedding_max_len = positional_embedding_max_len)
 #     
+#     criterion = torch.nn.MSELoss()    # mean-squared error for regression
+#     optimizer1 = torch.optim.Adam(network1.parameters(), lr=learning_rate)
+#     # optimizer2 = torch.optim.Adam(network2.parameters(), lr=learning_rate)
+#     optimizer3 = torch.optim.Adam(network3.parameters(), lr=learning_rate)
 # =============================================================================
-    criterion = torch.nn.MSELoss()    # mean-squared error for regression
-    optimizer1 = torch.optim.Adam(network1.parameters(), lr=learning_rate)
-    # optimizer2 = torch.optim.Adam(network2.parameters(), lr=learning_rate)
-    optimizer3 = torch.optim.Adam(network3.parameters(), lr=learning_rate)
     
-    print("Model 1 (Transformer) has %d parameters. Model 2 (Benchmark) has %d parameters. Model 3 (Pyraformer) has %d parameters" % (count_parameters(network1), count_parameters(network2), count_parameters(network3)))
+    print(networks.get_params_string())
     
-    train_losses1 = []
-    test_losses1  = []
-    train_losses2 = []
-    test_losses2  = []
-    train_losses3 = []
-    test_losses3  = []
-    
-    train_data, test_data = load_data("mocap/brownies1.c3d", batch_size)
+    train_data, test_data = load_data(("mocap/brownies1.c3d", "mocap/brownies3.c3d", "mocap/brownies4.c3d",
+                                       "mocap/eggs1.c3d", "mocap/eggs3.c3d", "mocap/eggs4.c3d", 
+                                       "mocap/pizza1.c3d", "mocap/pizza2.c3d", "mocap/pizza3.c3d", "mocap/pizza4.c3d", 
+                                       "mocap/salad1.c3d", "mocap/salad2.c3d", "mocap/salad3.c3d", "mocap/salad4.c3d", 
+                                       "mocap/sandwich1.c3d", "mocap/sandwich2.c3d", "mocap/sandwich3.c3d", "mocap/sandwich4.c3d", ), batch_size)
+    train_data.requires_grad = True
+    test_data.requires_grad = True
     
     print("Beginning training...")
     
     # Train the model
     for epoch in range(num_epochs):
-        network1.train()
-        network2.train()
-        network3.train()
-        outputs1 = network1(train_data[:, :-predict_length], min_seq_length)
-        outputs2 = network2(train_data[:, :-predict_length], min_seq_length)
-        outputs3 = network3(train_data[:, :-predict_length], min_seq_length)
-        
-        optimizer1.zero_grad()
-        # optimizer2.zero_grad()
-        optimizer3.zero_grad()
-        gc.collect()
-        
-        # obtain the loss function
-        loss1 = criterion(outputs1, train_data[:, min_seq_length+predict_length:])
-        loss2 = criterion(outputs2, train_data[:, min_seq_length+predict_length:])
-        loss3 = criterion(outputs3, train_data[:, min_seq_length+predict_length:])
-        
-        loss1.backward()
-        # loss2.backward()
-        loss3.backward()
-        
-        optimizer1.step()
-        # optimizer2.step()
-        optimizer3.step()
-        
-        train_losses1.append(loss1.item())
-        train_losses2.append(loss2.item())
-        train_losses3.append(loss3.item())
-        
-        gc.collect()
-        network1.eval()
-        network2.eval()
-        network3.eval()
-        
-        with torch.no_grad():
-            train_predict1 = network1(test_data[:, :-predict_length], min_seq_length)
-            train_predict2 = network2(test_data[:, :-predict_length], min_seq_length)
-            train_predict3 = network3(test_data[:, :-predict_length], min_seq_length)
-            test_loss1 = criterion(train_predict1, test_data[:, min_seq_length+predict_length:])
-            test_loss2 = criterion(train_predict2, test_data[:, min_seq_length+predict_length:])
-            test_loss3 = criterion(train_predict3, test_data[:, min_seq_length+predict_length:])
-            test_losses1.append(test_loss1.item())
-            test_losses2.append(test_loss2.item())
-            test_losses3.append(test_loss3.item())
-            if epoch % 1 == 0:
-                print("Epoch: %d, Transformer train loss: %1.5f, Transformer test loss: %1.5f, Benchmark train loss: %1.5f, Benchmark test loss: %1.5f, Pyraformer train loss: %1.5f, Pyraformer test loss: %1.5f" % (epoch, loss1.item(), test_loss1.item(), loss2.item(), test_loss2.item(), loss3.item(), test_loss3.item()))
+        networks.train(train_data, min_seq_length)
+        networks.test(test_data, min_seq_length)
+        print(networks.get_losses_string())
     
-    plt.plot(train_losses1, label = "Train (Transformer)")
-    plt.plot(test_losses1, label = "Test (Transformer)")
-    plt.plot(train_losses2, label = "Train (Benchmark)")
-    plt.plot(test_losses2, label = "Test (Benchmark)")
-    plt.plot(train_losses3, label = "Train (Pyraformer)")
-    plt.plot(test_losses3, label = "Test (Pyraformer)")
-    plt.legend()
-    plt.yscale('log')
-    plt.show()
-    # TODO torch.save(network1.state_dict(), "TrainedKitchenTransformer.pt")
-    # TODO torch.save(network2.state_dict(), "TrainedKitchenInformer.pt")
-    torch.save(network3.state_dict(), "TrainedKitchenPyraformer.pt")
+    networks.plot_losses_over_time()
+    networks.save_models(("TrainedKitchenTransformer.pt", "TrainedKitchenInformer.pt", None))
