f61683a99761003d5bb34593a6cb73a7d4b67304
Eddie Wei <eddie.y.wei@gmail.com>
2022-09-26 16:28:22 -0400

    edited a few things back

--- kitchen_pose_prediction/models/LSTM.py
+++ kitchen_pose_prediction/models/LSTM.py
@@ -15,30 +15,30 @@ class LSTMBenchmark(nn.Module):
         return self.layer(y)[0][:, min_seq_len:]
 
 # =============================================================================
-class TwoLayerLSTM(nn.Module):
-    def __init__(self, hidden_layers=64, frame_dimension = 1):
-        super(TwoLayerLSTM, self).__init__()
-        self.hidden_layers = hidden_layers
-        self.input_size = frame_dimension
-        # lstm1, lstm2, linear are all layers in the network
-        self.lstm1 = nn.LSTMCell(frame_dimension, self.hidden_layers)
-        self.lstm2 = nn.LSTMCell(self.hidden_layers, self.hidden_layers)
-        self.linear = nn.Linear(self.hidden_layers, frame_dimension)
-
-    def forward(self, y, pre_output_len=1):
-        outputs = []
-        h_t = torch.zeros(1, self.hidden_layers, dtype=torch.float32)
-        c_t = torch.zeros(1, self.hidden_layers, dtype=torch.float32)
-        h_t2 = torch.zeros(1, self.hidden_layers, dtype=torch.float32)
-        c_t2 = torch.zeros(1, self.hidden_layers, dtype=torch.float32)
-
-        for i, frame in enumerate(y.split(1, dim=0)):
-            h_t, c_t = self.lstm1(frame, (h_t, c_t)) # initial hidden and cell states
-            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2)) # new hidden and cell states
-            output = self.linear(h_t2) # output from the last FC layer
-            if i >= pre_output_len: outputs.append(output)
-
-        # transform list to tensor
-        outputs = torch.stack(outputs)
-        return outputs
+# class TwoLayerLSTM(nn.Module):
+#     def __init__(self, hidden_layers=64, frame_dimension = 1):
+#         super(TwoLayerLSTM, self).__init__()
+#         self.hidden_layers = hidden_layers
+#         self.input_size = frame_dimension
+#         # lstm1, lstm2, linear are all layers in the network
+#         self.lstm1 = nn.LSTMCell(frame_dimension, self.hidden_layers)
+#         self.lstm2 = nn.LSTMCell(self.hidden_layers, self.hidden_layers)
+#         self.linear = nn.Linear(self.hidden_layers, frame_dimension)
+#
+#     def forward(self, y, pre_output_len=1):
+#         outputs = []
+#         h_t = torch.zeros(1, self.hidden_layers, dtype=torch.float32)
+#         c_t = torch.zeros(1, self.hidden_layers, dtype=torch.float32)
+#         h_t2 = torch.zeros(1, self.hidden_layers, dtype=torch.float32)
+#         c_t2 = torch.zeros(1, self.hidden_layers, dtype=torch.float32)
+#
+#         for i, frame in enumerate(y.split(1, dim=0)):
+#             h_t, c_t = self.lstm1(frame, (h_t, c_t)) # initial hidden and cell states
+#             h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2)) # new hidden and cell states
+#             output = self.linear(h_t2) # output from the last FC layer
+#             if i >= pre_output_len: outputs.append(output)
+#
+#         # transform list to tensor
+#         outputs = torch.stack(outputs)
+#         return outputs
 # =============================================================================
